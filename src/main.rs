8af2dbb4f6a3637a725f90e774b33861cf773794
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-08-08 12:42:31 +0600

    feat: :hammer: finish project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        let mut guess = String::new();
+
+        println!("Please input your guess: ");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("The secret number is: {}", secret_number);
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => println!("{}", "You win!".green()),
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => {
+                println!("{}", "Too big!".red());
+                break;
+            }
+        }
+    }
 }
